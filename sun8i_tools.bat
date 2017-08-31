1ad0571c4d14f9df4c511f85a72c6dae24bbf0ac
Doctor_Titi <DoctorTiti@users.noreply.github.com>
2017-09-01 01:25:34 +0200

    01/09/2017

---
+++ sun8i_tools.bat
@@ -0,0 +1,98 @@
+@echo off
+set cls=1
+
+:menu
+set type=0
+color 0A
+
+IF %cls% == 1 cls
+echo.
+echo Welcome to sun8i_tools by Doctor_Titi
+echo.
+echo.
+echo  ___________________________________________________
+echo "         type 0 to show available commands         "
+echo  ---------------------------------------------------
+echo.
+echo.
+
+REM Selector condition
+
+set /p type=Make a choice:
+if %type% == 0 goto :help
+if %type% == 1 goto :SuperSu
+if %type% == 2 goto :CLS
+
+REM Loop
+cls
+color 0C
+echo.
+echo.                     (%type%) is a
+echo  ___________________________________________________
+echo "                  Wrong Choice.                    "
+echo  ---------------------------------------------------
+echo.
+pause|echo Press any key!
+cls
+goto :menu
+
+:CLS
+echo.
+echo This menu will allow you to erase text after showing up new things. (clear/CLS)
+echo 0. Disable CLS
+echo 1. Enabke CLS
+set /p typecls=Make a choice: 
+
+if %typecls% == 0 goto :disable
+if %typecls% == 1 goto :enable
+goto :CLS
+
+:disable
+set cls=
+:enable
+
+
+:help
+echo.
+echo  ___________________________________________________
+echo " 0. Show this menu                                 "
+echo " 1. Install Supersu v2.82 & busybox 1.21 via ADB   "
+echo  ---------------------------------------------------
+echo.
+pause|echo Press any key.
+REM go back to the menu
+goto :menu
+
+
+:SuperSu
+echo Installing BusyBox 1.21...
+echo.
+echo Pushing binaries in temp folder.
+adb push .\Bin\1\busybox /data/local/tmp/
+
+echo.
+echo Mount /system as R/W.
+adb shell "su -c mount -o remount -r -w /system"
+
+echo.
+echo Removing old busybox.
+adb shell "su -c rm /system/bin/busybox"
+
+echo.
+echo Copying binaries into system folder.
+adb shell "su -c cp /data/local/tmp/busybox /system/bin/"
+
+echo.
+echo Changin permissions.
+adb shell "su -c chmod 777 /system/bin/busybox"
+
+ping -n 3 127.0.0.1 > null
+
+
+
+
+
+
+set cls=0
+goto :menu
+
